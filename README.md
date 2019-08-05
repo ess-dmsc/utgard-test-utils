7d07ddedd93a1ed8516023d5ac9792ef6cc424fb
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-08-05 14:20:29 +0200

    Createw package structure

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # utgard-test-utils
 
 Utilities for test automation at Utgård
+
+
+## Installation
+
+Run
+
+    pip install .
+
+
+## Developing
+
+You can make the code available to your Python installation by performing an
+editable install with
+
+    pip install -e .
+
+To run the tests, install the development requirements by running
+
+    pip install -r requirements-dev.txt
+
+The tests can then be run against the installed package with
+
+    pytest [--cov=src/versionit --cov-report=xml:<path>] [--junit-xml=<path>]
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="utgardtests",
+    version="0.0.1",
+    author="Afonso Mukai",
+    author_email="afonso.mukai@esss.se",
+    description="Utilities for test automation at Utgård",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ess-dmsc/utgard-test-utils",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "confluent-kafka==1.1.0",
+        "fabric==2.4.0",
+        "h5py==2.9.0",
+        "matplotlib==3.1.0",
+        "numpy==1.16.4",
+        "pandas==0.24.2",
+        "requests==2.22.0",
+        "scikit-learn==0.21.2",
+        "scipy==1.3.0",
+        "statsmodels==0.10.0",
+        "tables==3.5.2",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
