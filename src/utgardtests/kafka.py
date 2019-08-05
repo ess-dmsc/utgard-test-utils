33a3bef224f8c72fb7029fc579a4533bd2fc4884
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-08-05 19:32:05 +0200

    Add KafkaAdmin

--- src/utgardtests/kafka.py
+++ src/utgardtests/kafka.py
@@ -1,4 +1,62 @@
-class Kafka:
+import concurrent.futures
+import time
+import confluent_kafka.admin
 
-    def __init__(self):
-        self.value = 1
+
+class KafkaAdmin:
+    def __init__(self, bootstrap_servers_admin, log):
+        self.kafka_admin_client = confluent_kafka.admin.AdminClient(
+            {"bootstrap.servers": bootstrap_servers_admin}
+        )
+        self.log = log
+
+    def topic_exists(self, topic_name):
+        cluster_metadata = self.kafka_admin_client.list_topics()
+        return topic_name in cluster_metadata.topics
+
+    def create_kafka_topic(self, topic_name, replica_assignment):
+        num_partitions = len(replica_assignment)
+        new_topic = confluent_kafka.admin.NewTopic(
+            topic=topic_name,
+            num_partitions=num_partitions,
+            replica_assignment=replica_assignment,
+        )
+        r = self.kafka_admin_client.create_topics([new_topic])
+        _ = concurrent.futures.wait(
+            r.values(), return_when=concurrent.futures.FIRST_EXCEPTION
+        )
+        e = r[new_topic.topic].exception()
+        if e is not None:
+            self._add_to_log(
+                "Exception '{}' when trying to create topic '{}'".format(
+                    e.args[0], topic_name
+                )
+            )
+            raise e
+        self._add_to_log(
+            "Created topic '{}' with replica assignment '{}'".format(
+                topic_name, replica_assignment
+            ),
+            True,
+        )
+
+    def delete_kafka_topic(self, topic_name):
+        r = self.kafka_admin_client.delete_topics([topic_name])
+        _ = concurrent.futures.wait(
+            r.values(), return_when=concurrent.futures.FIRST_EXCEPTION
+        )
+        e = r[topic_name].exception()
+        if e is not None:
+            self._add_to_log(
+                "Exception '{}' when trying to delete topic '{}'".format(
+                    e.args[0], topic_name
+                )
+            )
+            raise e
+        self._add_to_log("Deleted topic '{}'".format(topic_name), True)
+
+    def _add_to_log(self, message, print_message=False):
+        t = round(time.time())
+        self.log.put("{}: {}".format(t, message))
+        if print_message:
+            print(message)
--- tests/utgardtests/kafka_test.py
+++ tests/utgardtests/kafka_test.py
@@ -1,8 +1,106 @@
+import concurrent.futures
+import queue
+import confluent_kafka.admin
+import pytest
 import utgardtests.kafka
 
 
-class TestKafka():
+class PartitionMetadataStub:
+    pass
 
-    def test_value(self):
-        kafka = utgardtests.kafka.Kafka()
-        assert kafka.value == 1
+
+class TopicMetadataStub:
+    def __init__(self, topic):
+        self.topic = topic
+
+
+class ClusterMetadataStub:
+    topics = {
+        "test-topic": TopicMetadataStub("test-topic"),
+        "performance-topic": TopicMetadataStub("performance-topic"),
+    }
+
+
+class KafkaAdminStub:
+    @staticmethod
+    def list_topics():
+        return ClusterMetadataStub()
+
+    @staticmethod
+    def create_topics(new_topics):
+        topic_futures = {}
+        for new_topic in new_topics:
+            f = concurrent.futures.Future()
+            if new_topic.topic == "good-topic":
+                f.set_result(0)
+            else:
+                f.set_result(1)
+                f.set_exception(Exception("An exception happened"))
+            topic_futures[new_topic.topic] = f
+
+        return topic_futures
+
+    @staticmethod
+    def delete_topics(topics):
+        topic_futures = {}
+        for topic in topics:
+            f = concurrent.futures.Future()
+            if topic == "good-topic":
+                f.set_result(0)
+            else:
+                f.set_result(1)
+                f.set_exception(Exception("An exception happened"))
+            topic_futures[topic] = f
+
+        return topic_futures
+
+
+class TestKafkaAdmin:
+    def get_mock_kafka_admin(self, *args, **kwargs):
+        return KafkaAdminStub()
+
+    @pytest.fixture
+    def kafka_admin_client(self, monkeypatch):
+        monkeypatch.setattr(
+            confluent_kafka.admin, "AdminClient", self.get_mock_kafka_admin
+        )
+        kafka_admin_client = utgardtests.kafka.KafkaAdmin(
+            "localhost:9092", queue.Queue()
+        )
+        return kafka_admin_client
+
+    def test_topic_exists(self, kafka_admin_client):
+        assert kafka_admin_client.topic_exists("test-topic")
+        assert kafka_admin_client.topic_exists("performance-topic")
+        assert not kafka_admin_client.topic_exists("nonexistent-topic")
+
+    def test_create_kafka_topic_exception(self, kafka_admin_client):
+        replica_assignment = [[1, 2], [3, 5]]
+        kafka_admin_client.create_kafka_topic("good-topic", replica_assignment)
+        with pytest.raises(Exception):
+            kafka_admin_client.create_kafka_topic(
+                "bad-topic", replica_assignment
+            )
+
+    def test_delete_kafka_topic_exception(self, kafka_admin_client):
+        kafka_admin_client.delete_kafka_topic("good-topic")
+        with pytest.raises(Exception):
+            kafka_admin_client.delete_kafka_topic("bad-topic")
+
+    def test_log_length(self, kafka_admin_client):
+        assert kafka_admin_client.log.empty()
+
+        replica_assignment = [[1, 2], [3, 5]]
+        kafka_admin_client.create_kafka_topic("good-topic", replica_assignment)
+        with pytest.raises(Exception):
+            kafka_admin_client.create_kafka_topic(
+                "bad-topic", replica_assignment
+            )
+        kafka_admin_client.delete_kafka_topic("good-topic")
+
+        n = 0
+        while not kafka_admin_client.log.empty():
+            kafka_admin_client.log.get()
+            n = n + 1
+
+        assert n == 3
