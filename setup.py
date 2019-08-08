1605995fc2238cb47be241a102b10ec083310f5e
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-08-08 19:08:43 +0200

    Create version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="utgardtests",
-    version="0.0.0",
+    version="0.2.0",
     author="Afonso Mukai",
     author_email="afonso.mukai@esss.se",
     description="Utilities for test automation at Utgård",
