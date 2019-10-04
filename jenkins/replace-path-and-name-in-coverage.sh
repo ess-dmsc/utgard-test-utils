a4c257eae6aff3a5fd443aab1950e0927b11b750
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-10-04 22:37:57 +0200

    Update Jenkins coverage script

--- jenkins/replace-path-and-name-in-coverage.sh
+++ jenkins/replace-path-and-name-in-coverage.sh
@@ -7,4 +7,4 @@ coverage_report_path=$4
 
 sed -i"" -e "s|$source_path_to_replace|$new_source_path_value|" $coverage_report_path
 sed -i"" -e "s| name=\"virtualenv.*\"| name=\"$package_name\"|" $coverage_report_path
-sed -i"" -e "s| filename=\"virtualenv.*/| filename=\"|" $coverage_report_path
+sed -i"" -e "s| filename=\"virtualenv.*/$package_name/| filename=\"|" $coverage_report_path
